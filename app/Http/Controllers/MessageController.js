80f5f2de4047ac14576f2006f77a9cb2b681cab1
Christopher Duke <cd.duke@gmail.com>
2016-08-29 14:47:55 -0500

    parent profile message button directs to message

--- app/Http/Controllers/MessageController.js
+++ app/Http/Controllers/MessageController.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 const Message = use('App/Model/Message');
--- app/Model/Message.js
+++ app/Model/Message.js
@@ -1,16 +1,18 @@
-'use strict'
+'use strict';
 
-const Lucid = use('Lucid')
+const Lucid = use('Lucid');
 
 class Message extends Lucid {
 
-
-  participant() {
-    return this.belongsTo('App/Model/Participant', 'id', 'participant_id');
+  messages() {
+    return this.hasMany('App/Model/Message', 'id', 'conversation_id');
   }
   conversation() {
     return this.belongsTo('App/Model/Conversation', 'id', 'conversation_id');
   }
+  participants() {
+    return this.hasMany('App/Model/Participant', 'id', 'conversation_id');
+  }
 }
 
-module.exports = Message
+module.exports = Message;
