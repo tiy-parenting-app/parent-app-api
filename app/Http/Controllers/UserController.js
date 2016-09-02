40bab0b351c3be489f9d566cb4765032dd300b37
Ryan Tablada <ryan.tablada@gmail.com>
2016-09-01 19:17:40 -0500

    fix: no such thing as a like table

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -15,7 +15,7 @@ class UserController {
           query.where('ratings.user_id', request.authUser.id);
         })
         .scope('profile.likes', (query) => {
-          query.where('like.user_id', request.authUser.id);
+          query.where('likes.user_id', request.authUser.id);
         })
         .where('users.account_type', typeFilter)
         .fetch();
